9cbc042f2a62827a4254ebfc92dd86da840b7c97
Tinaye Katemba <tinaye3434@gmail.com>
2023-06-17 08:05:45 +0200

    Update function complete

--- update.php
+++ update.php
@@ -15,8 +15,52 @@ if (!$user) {
     exit;
 }
 
-echo '<pre>';
-var_dump($user);
-echo '</pre>';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    updateUser($_POST, $userId);
+}
+
+?>
+
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h3>Update User <b><?php echo $user['name'] ?></b></h3>
+        </div>
+        <div class="card-body">
+            <form method="post" enctype="multipart/form-data"
+                  action="">
+
+                <div class="form-group">
+                    <label>Name</label>
+                    <input name="name" value="<?php echo $user['name'] ?>" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label>Username</label>
+                    <input name="username" value="<?php echo $user['username'] ?>" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label>Email</label>
+                    <input name="email" value="<?php echo $user['email'] ?>" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Phone</label>
+                    <input name="phone" value="<?php echo $user['phone'] ?>" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Website</label>
+                    <input name="website" value="<?php echo $user['website'] ?>" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Image</label>
+                    <input name="name" type="file" class="form-control">
+                </div>
+
+                <button class="btn btn-success">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
 
 
--- users.php
+++ users.php
@@ -23,7 +23,15 @@ function createUser($data)
 
 function updateUser($data, $id)
 {
+    $users = getUsers();
+    foreach ($users as $i => $user) {
+        if ($user['id'] == $id){
+            $users[$i] = array_merge($user, $data);
 
+            header("Location: index.php");
+        }
+    }
+    file_put_contents(__DIR__.'/users.json', json_encode($users));
 }
 
 function deleteUser($id)
